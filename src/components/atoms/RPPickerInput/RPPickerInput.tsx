1f0891c9425cbbbfeff810c2777b1011f1221c84
JaninduDiz <janindu001@gmail.com>
2024-09-07 01:00:20 +0530

    Refactor RPPickerInput component and add react-native-element-dropdown dependency

--- src/components/atoms/RPPickerInput/RPPickerInput.tsx
+++ src/components/atoms/RPPickerInput/RPPickerInput.tsx
@@ -1,91 +1,105 @@
-// import React from "react";
-// import { View, StyleSheet, Text } from "react-native";
-// import { Picker } from "@react-native-picker/picker";
-// import { Icon } from "@rneui/themed";
-// import { BASIC_COLORS } from "../../../utils/constants/styles";
+import React, { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import { Icon } from "@rneui/themed";
+import { BASIC_COLORS } from "../../../utils/constants/styles";
 
-// type RPPickerInputProps = {
-//   label?: string;
-//   selectedValue: string;
-//   onValueChange: (itemValue: string) => void;
-//   options: { label: string; value: string }[];
-//   pickerStyle?: object;
-//   error?: string;
-// };
+type RPPickerInputProps = {
+  label?: string;
+  selectedValue: string;
+  onValueChange: (itemValue: string) => void;
+  options: { label: string; value: string }[];
+  pickerStyle?: object;
+  error?: string;
+};
 
-// const RPPickerInput = ({
-//   label,
-//   selectedValue,
-//   onValueChange,
-//   options,
-//   pickerStyle,
-//   error,
-// }: RPPickerInputProps) => {
-//   return (
-//     <View style={styles.container}>
-//       {/* Label */}
-//       {label && <Text style={styles.label}>{label}</Text>}
+const RPPickerInput = ({
+  label,
+  selectedValue,
+  onValueChange,
+  options,
+  pickerStyle,
+  error,
+}: RPPickerInputProps) => {
+  const [isFocus, setIsFocus] = useState(false);
 
-//       {/* Picker Container */}
-//       <View style={[styles.pickerContainer, pickerStyle]}>
-//         <Picker
-//           selectedValue={selectedValue}
-//           onValueChange={onValueChange}
-//           style={styles.picker}
-//         >
-//           {options.map((option, index) => (
-//             <Picker.Item
-//               key={index}
-//               label={option.label}
-//               value={option.value}
-//             />
-//           ))}
-//         </Picker>
+  return (
+    <View style={styles.container}>
+      {/* Label */}
+      {label && <Text style={styles.label}>{label}</Text>}
 
-//         {/* Optional Dropdown Icon from RNE */}
-//         <Icon
-//           name="arrow-drop-down"
-//           type="material"
-//           color={BASIC_COLORS.TERTIARY}
-//         />
-//       </View>
+      {/* Dropdown Container */}
+      <Dropdown
+        style={[
+          styles.dropdown,
+          pickerStyle,
+          isFocus && { borderColor: BASIC_COLORS.PRIMARY },
+        ]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        iconStyle={styles.iconStyle}
+        data={options}
+        labelField="label"
+        valueField="value"
+        placeholder={!isFocus ? "Select item" : "..."}
+        value={selectedValue}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(item) => {
+          onValueChange(item.value);
+          setIsFocus(false);
+        }}
+        renderRightIcon={() => (
+          <Icon
+            name="arrow-drop-down"
+            type="material"
+            color={BASIC_COLORS.PRIMARY}
+          />
+        )}
+      />
 
-//       {/* Error Message */}
-//       {error && <Text style={styles.errorText}>{error}</Text>}
-//     </View>
-//   );
-// };
+      {/* Error Message */}
+      {error && <Text style={styles.errorText}>{error}</Text>}
+    </View>
+  );
+};
 
-// const styles = StyleSheet.create({
-//   container: {
-//     marginBottom: 15,
-//   },
-//   label: {
-//     color: BASIC_COLORS.FONT_PRIMARY,
-//     fontWeight: "500",
-//     fontSize: 16,
-//     marginBottom: 5,
-//   },
-//   pickerContainer: {
-//     flexDirection: "row",
-//     alignItems: "center",
-//     borderColor: BASIC_COLORS.TERTIARY,
-//     borderWidth: 1,
-//     borderRadius: 10,
-//     backgroundColor: BASIC_COLORS.WHITE,
-//     overflow: "hidden",
-//     height: 45,
-//     justifyContent: "space-between",
-//   },
-//   picker: {
-//     flex: 1,
-//     height: "100%",
-//   },
-//   errorText: {
-//     color: BASIC_COLORS.ERROR,
-//     fontSize: 12,
-//     marginTop: 5,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    //marginBottom: 15,
+  },
+  label: {
+    color: BASIC_COLORS.FONT_PRIMARY,
+    fontWeight: "400",
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  dropdown: {
+    borderColor: BASIC_COLORS.LIGHT_GRAY,
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 45,
+    backgroundColor: BASIC_COLORS.WHITE,
+    paddingHorizontal: 10,
+    justifyContent: "center",
+  },
+  placeholderStyle: {
+    fontSize: 16,
+    color: BASIC_COLORS.FONT_SECONDARY,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    color: BASIC_COLORS.FONT_PRIMARY,
+  },
+  iconStyle: {
+    width: 24,
+    height: 24,
+  },
+  errorText: {
+    color: BASIC_COLORS.ERROR,
+    fontSize: 12,
+    marginTop: 5,
+  },
+});
 
-// export default RPPickerInput;
+export default RPPickerInput;
