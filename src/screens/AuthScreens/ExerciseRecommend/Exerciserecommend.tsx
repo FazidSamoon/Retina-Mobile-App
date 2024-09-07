e178ec827f5406670c132e22697a7b4c2d2edef7
JaninduDiz <janindu001@gmail.com>
2024-09-08 04:20:11 +0530

    Refactor ExerciseRecommend component

--- src/screens/AuthScreens/ExerciseRecommend/Exerciserecommend.tsx
+++ src/screens/AuthScreens/ExerciseRecommend/Exerciserecommend.tsx
@@ -1,12 +1,24 @@
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text } from "react-native";
 import React from "react";
-import MealsRecommendContainer from "../../../components/organisms/MealsRecommendContainer/MealsRecommendContainer";
+import VisionHomeScreenTopAppBar from "../../../components/molecules/VisionHomeScreenTopAppBar/VisionHomeScreenTopAppBar";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { AuthScreensParamList } from "../../../navigators/RootNavigator/types";
 
 const ExerciseRecommend = () => {
+  const navigation = useNavigation<NavigationProp<AuthScreensParamList>>();
+  const navigateTo = () => {
+    navigation.navigate("RecommendHome");
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      {/*  <Temporarily /> */}
-      <MealsRecommendContainer />
+      <VisionHomeScreenTopAppBar
+        header="Exercise Recommender"
+        navigateTo={navigateTo}
+      />
+      <View style={styles.card}>
+        <Text style={styles.text}>Coming Soon</Text>
+      </View>
     </SafeAreaView>
   );
 };
@@ -21,5 +33,24 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     height: "100%",
     backgroundColor: "white",
+  },
+  card: {
+    width: "90%",
+    height: 150,
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    margin: 20,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333",
   },
 });
